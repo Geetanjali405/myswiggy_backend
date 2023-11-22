47fa6607191d6e1c142982bb06bce8545e428ce8
Geetanjali <geetbgs04@gmail.com>
2023-11-22 15:28:32 +0530

    Add files via upload

---
+++ src/test/java/com/example/myswiggy/MyswiggyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myswiggy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyswiggyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
